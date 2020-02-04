74356b4d05b02dc3b494f24ede8bc23ee490c066
Jack Lu <jacklu@jacklu.me>
2020-02-04 13:53:38 +0800

    +Content-Type

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 var express = require('express');
 function App() {
-	var text = {};
+	var content = {};
 	var app = express();
-	app.use(express.text());
+	app.use(express.text({ type: "*/*" }));
 	app.put('*', (req, res) => {
-		res.status(req.url in text ? 204 : 201);
-		text[req.url] = req.body;
+		res.status(req.url in content ? 204 : 201);
+		content[req.url] = {
+			type: req.get('Content-Type'),
+			body: req.body
+		};
 		res.end();
 	});
 	app.get('*', (req, res) => {
-		if (req.url in text)
-			res.status(200).set('Content-Type', 'text/plain').send(text[req.url]);
-		else
+		if (req.url in content) {
+			var _content = content[req.url];
+			res.status(200).set('Content-Type', _content.type || 'text/plain').send(_content.body);
+		} else
 			res.status(404).end();
 	});
 	return app;

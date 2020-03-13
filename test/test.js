de33be189f8745476d0a68107edd0d1561a87441
Jack Lu <jacklu@jacklu.me>
2020-03-13 23:55:13 +0800

    +test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var request = require('request');
+var App = require('..');
+it('wypiwyg', function (done) {
+	var app = App();
+	app.listen(8000, () => {
+		request.put('http://localhost:8000/a', {
+			headers: { 'Content-Type': 'a/a' },
+			body: 'a'
+		}, () => {
+			request.get('http://localhost:8000/a', (error, response) => {
+				var contentType = require('content-type').parse(response.headers['content-type']);
+				assert.equal(contentType.type, 'a/a');
+				assert.equal(response.body, 'a');
+				done();
+			});
+		});
+	});
+});

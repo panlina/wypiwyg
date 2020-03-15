b9b1d5c579214baafb79c0bfd7dfab59101cb30b
Jack Lu <jacklu@jacklu.me>
2020-03-15 23:45:40 +0800

    test: +404, 20x

--- test/test.js
+++ test/test.js
@@ -18,3 +18,32 @@ it('wypiwyg', function (done) {
 	});
 	var port = server.address().port;
 });
+it('404', function (done) {
+	var app = App();
+	var server = app.listen(() => {
+		request.get(`http://localhost:${port}/a`, (error, response) => {
+			assert.equal(response.statusCode, 404);
+			done();
+		});
+	});
+	var port = server.address().port;
+});
+it('20x', function (done) {
+	var app = App();
+	var server = app.listen(() => {
+		request.put(`http://localhost:${port}/a`, {
+			headers: { 'Content-Type': 'a/a' },
+			body: 'a'
+		}, (error, response) => {
+			assert.equal(response.statusCode, 201);
+			request.put(`http://localhost:${port}/a`, {
+				headers: { 'Content-Type': 'a/a' },
+				body: 'b'
+			}, (error, response) => {
+				assert.equal(response.statusCode, 204);
+				done();
+			});
+		});
+	});
+	var port = server.address().port;
+});

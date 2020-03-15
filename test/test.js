0a6f06f8b7036bccc69d338d27581d0980e2b4e6
Jack Lu <jacklu@jacklu.me>
2020-03-15 23:37:19 +0800

    test: use random port

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ var request = require('request');
 var App = require('..');
 it('wypiwyg', function (done) {
 	var app = App();
-	app.listen(8000, () => {
-		request.put('http://localhost:8000/a', {
+	var server = app.listen(() => {
+		request.put(`http://localhost:${port}/a`, {
 			headers: { 'Content-Type': 'a/a' },
 			body: 'a'
 		}, () => {
-			request.get('http://localhost:8000/a', (error, response) => {
+			request.get(`http://localhost:${port}/a`, (error, response) => {
 				var contentType = response.headers['content-type'];
 				assert.equal(contentType, 'a/a');
 				assert.equal(response.body, 'a');
@@ -16,4 +16,5 @@ it('wypiwyg', function (done) {
 			});
 		});
 	});
+	var port = server.address().port;
 });
